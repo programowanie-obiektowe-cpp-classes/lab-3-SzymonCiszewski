0dcef25d5c897fa64a3f0550ad5ab50ace7c3117
Szymon Ciszewski <szymon526.ciszewski@wp.pl>
2024-10-29 19:55:56 +0100

    Zejsciowka 3.

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,22 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    int przetransportowany_towar = 0;
+    int wyprodukowane_zaglowce  = 0;
+    if (towar > 0) {
+        while (1) {
+            Stocznia Gdynia;
+            auto statek = Gdynia();
+            przetransportowany_towar += statek->transportuj();
+            if (dynamic_cast< Zaglowiec* >(statek))
+                wyprodukowane_zaglowce++;
+
+            delete statek;
+            if (przetransportowany_towar >= towar)
+                break;
+
+        }
+    }
+
+    return wyprodukowane_zaglowce;
 }
